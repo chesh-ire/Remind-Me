98a8ed44eb006d32a13ce0ca59b4c79fec7b04c4
Cheshire <126002367+SSHAARVARI@users.noreply.github.com>
2023-12-20 00:08:00 +0530

    interfaces updated

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -14,6 +14,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
